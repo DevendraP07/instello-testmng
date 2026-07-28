ca0bf71f861b870fc1e619fadf8170c0e2d81bd8
x-sss-x <manu48617@gmail.com>
2026-07-28 10:15:35 +0530

    feat: add getAssessment to queries.ts

--- convex/tests/queries.ts
+++ convex/tests/queries.ts
@@ -1,1 +1,19 @@
+import { query } from "#_generated/server";
+import { ConvexError, v } from "convex/values";
 
+/** **Get assessment schema** */
+export const getAssessmentSchema = query({
+  args: {
+    id: v.id("assessmentSchemas"),
+  },
+  returns: {
+    _id: v.id("assessmentSchema"),
+    name: v.string(),
+    description: v.optional(v.string()),
+    createdAt: v.number(),
+    updatedAt: v.optional(v.string()),
+  },
+  handler: async (ctx, args) => {
+    throw new ConvexError("Not implemented yet");
+  },
+});
