a716510d5f7265516b76b237b4894bc303730965
x-sss-x <manu48617@gmail.com>
2026-07-28 14:34:47 +0530

    refactor: update assessment function & validtors

--- convex/tests/model/assessmentComponent.ts
+++ convex/tests/model/assessmentComponent.ts
@@ -1,28 +1,35 @@
-import { ConvexError } from "convex/values";
 import { Doc, Id } from "#_generated/dataModel";
-import { DbWriter, DbReader,lastorderIdx } from "#helpers/types";
+import { DbWriter, lastorderIdx } from "#helpers/types";
 
 /** Create assessment component*/
 export async function create(
   db: DbWriter,
-  body: Pick<Doc<"assessmentComponents">, "name" | "totalAllotedMarks"| "passingMarks" | "orderIdx">,
+  body: Pick<
+    Doc<"assessmentComponents">,
+    "name" | "totalAllotedMarks" | "passingMarks" | "orderIdx"
+  >,
 ) {
-    return await db.insert("assessmentComponents", {
-      ...body,
-      createdAt: Date.now(),
-      orderIdx: lastorderIdx+1,
-    });
+  return await db.insert("assessmentComponents", {
+    ...body,
+    createdAt: Date.now(),
+    orderIdx: lastorderIdx + 1,
+  });
 }
-/**
- **Update assessment component**
- */
+
+/** Update assessment component */
 export async function update(
-    db:DbWriter,
-    id:Id<"assessmentComponents">,
-    body: Pick<Doc<"assessmentComponents">,"name" | "totalAllotedMarks" | "passingMarks" >,
-){
-        return await db.patch(id,{ 
-            ...body,
-            updatedAt:Date.now(),
-        });
-    }
+  db: DbWriter,
+  id: Id<"assessmentComponents">,
+  body: Partial<
+    Pick<
+      Doc<"assessmentComponents">,
+      "name" | "totalAllotedMarks" | "passingMarks"
+    >
+  >,
+) {
+  return await db.patch(id, {
+    ...body,
+    updatedAt: Date.now(),
+  });
+}
+
--- convex/tests/validators.ts
+++ convex/tests/validators.ts
@@ -1,13 +1,24 @@
 import { v } from "convex/values";
 
-export const InsertAssessmentSchema = v.object({
+export const insertAssessmentSchemaValidator = v.object({
   name: v.string(),
   description: v.optional(v.string()),
 });
 
-export const InsertAssessmentComponent = v.object({
+export const insertAssessmentComponentValidator = v.object({
   name: v.string(),
   totalAllotedMarks: v.number(),
   passingMarks: v.number(),
   orderIdx: v.number(),
+});
+
+export const updateAssessmentComponentValidator = v.object({
+  id: v.id("assessmentComponents"),
+  body: v
+    .object({
+      name: v.string(),
+      passingMarks: v.number(),
+      totalAllotedMarks: v.number(),
+    })
+    .partial(),
 });
